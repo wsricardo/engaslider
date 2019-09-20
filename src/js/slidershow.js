b76ce32c6cf8dfdbcaefa869050c38b8ef8e3b80
Wandeson Ricardo <wandeson.math@gmail.com>
2019-09-20 09:53:32 -0300

    Update css, js.

--- src/js/slidershow.js
+++ src/js/slidershow.js
@@ -1,12 +1,39 @@
+/*MIT License
+
+Copyright (c) 2019 Wandeson Ricardo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
 var slideIndex, autoplay, slides, numberSlides;
+var slNextBtn, slPreviousBtn;
 
 // Init slider attributes
 function initSlider(play) {
     // Init vars
     slideIndex = 0;
-    autoplay = play;
+    autoplay = play;	
+	slNextBtn = document.getElementsByClassName("slider-next-button");
+	slPreviousBtn = document.getElementsByClassName("slider-previous-button");
     slides = document.getElementsByClassName("slider-image");
+
     numberSlides = slides.length;
+
     if ( autoplay ) {
         autoSlide();
     } else {
@@ -24,9 +51,12 @@ function setAutoPlay(){
 function showImage() {
     var j = 0;
 	var k = 0;
+	var offsetHeight;
 
     if  ( slideIndex > numberSlides - 1 ) { slideIndex = 0;}
     if ( slideIndex < 0 ) { slideIndex = slides.length - 1;}
+
+	offsetHeight = slides[slideIndex].getElementsByTagName("img")[0].naturalHeight;
 
     //*Debug*
     console.log(slides);
@@ -37,6 +67,11 @@ function showImage() {
     for (j = 0; j <= numberSlides - 1; j++) {
         slides[j].style.display = "none";
     }
+
+	//slNextBtn.style.top = '"'+offsetHeight+'px'+'"';
+	//slPreviousBtn.style.top = '"'+offsetHeight+'px'+'"';
+
+	console.log("slides[]. "+ offsetHeight );
     slides[slideIndex].style.display = "block";
     slideIndex++;
 
